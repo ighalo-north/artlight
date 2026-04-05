b18d17a7649fc4fa86420d5ba74953133fb14336
jrosenbaum422 <jackrrosenbaum@gmail.com>
2026-04-05 00:16:57 -0400

    Longevity coxme model work

--- scripts/Longevity_analysis.R
+++ scripts/Longevity_analysis.R
@@ -7,6 +7,7 @@ library(tidyr)
 library(dplyr) 
 library(janitor) 
 library(survminer)
+library(ggplot2)
 
 longev_data <- (read_excel("data/Longevity_Final.xlsx") 
                 |> mutate(across(where(is.character),as.factor))
@@ -17,21 +18,32 @@ longev_data$status <- ifelse(longev_data$fly_lifespan == "8",0,1)
 
 head(longev_data)
 
-Surv(longev_data$fly_lifespan, event=longev_data$status) # Tentative model, probably not properly done!
-
-
-m1 <- coxme(Surv(fly_lifespan, event=status) ~ treatment*sex + (1|treatment:(lineage/vial)), data=longev_data) 
-
-
+m1 <- coxme(Surv(fly_lifespan, event=status) ~ treatment*sex + (1|treatment/lineage/vial), data=longev_data) 
 
 summary(m1)
-Anova(m1)
+performance(m1) #does not work with cox mixed effects regression, it needs to be done manually
 
-fit <- survfit(Surv(fly_lifespan, event=status) ~ treatment, data=longev_data)
+#Schoenfeld Residuals test - check proportional hazards assumptions
+test_ph <- cox.zph(m1)
+ggcoxzph(test_ph)
 
+#Check for linearity using martingale residuals - not working:( not meant for coxme I think
+res_mart <- residuals(m1, type = "martingale")
+ggplot(data = longev_data, aes(x = status, y = res_mart)) +
+  geom_point() +
+  geom_smooth(method = "loess") +
+  labs(title = "Martingale Residuals vs Covariate")
+
+#Plotting deviance residuals - not working:(
+res_dev <- residuals(m1, type = "deviance")
+plot(res_dev, ylab = "Deviance Residuals", main = "Outlier Detection")
+abline(h = 0, lty = 2)
+
+
+fit <- survfit(Surv(fly_lifespan, event=status) ~ treatment, data=longev_data) #fit for the purpose of making the figure
 
 ggplot1 <- ggsurvplot(fit, size = 2, censor = FALSE, legend = c(.25,.50),legend.title = "Treatment",
-                      legend.labs = c("Selection","Control"), ggtheme = theme(title = element_text(size=30), axis.title.x = element_text(size=30), axis.title.y = element_text(size=30),
+                      legend.labs = c("Control","Selection"), ggtheme = theme(title = element_text(size=30), axis.title.x = element_text(size=30), axis.title.y = element_text(size=30),
                                                                               legend.text = element_text(size=25), axis.text = element_text(size=18), 
                                                                               legend.title = element_text(size=25), panel.background = element_rect(fill= FALSE),
                                                                               axis.line = element_line(), plot.title = element_text(size=35, hjust = 0.5), 

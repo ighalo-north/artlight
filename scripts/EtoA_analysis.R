c18d82782e323ea0c1f78563ddf13b836de5c99c
jrosenbaum422 <jackrrosenbaum@gmail.com>
2026-04-16 15:14:25 -0400

    inferential plots +

--- scripts/EtoA_analysis.R
+++ scripts/EtoA_analysis.R
@@ -6,6 +6,8 @@ library(dplyr)
 library(janitor) 
 library(ggplot2)
 library(performance)
+library(emmeans)
+library(car)
 
 etoa_data <- readRDS("data/etoa_data_clean.rds")
 
@@ -16,16 +18,46 @@ etoa_data <- transform(etoa_data,
                     trt_lin = interaction(treatment, lineage),
                     trt_lin_vial = interaction(treatment, lineage, vial))
 
-etoa_survival <- glmer(eclosed ~ treatment + (1|trt_lin) + (1|trt_lin_vial), family = "binomial", data=etoa_data)
+etoa_eclosion <- glmer(eclosed ~ treatment + (1|trt_lin) + (1|trt_lin_vial), family = "binomial", data=etoa_data)
 
-summary(etoa_survival)
-check_model(etoa_survival) 
+summary(etoa_eclosion)
+check_model(etoa_eclosion) 
 
 with(etoa_data, 
      table(trt_lin_vial, eclosed)) 
 #the influential observations identified by check_model are the instances where only one or two flies from that vial did not eclose.
 #this makes sense, and I don't think is too concerning.
 
+#Inferential plot
+eclosion_estimates <- emmeans(etoa_eclosion, ~ treatment, type = "response") 
+eclosion_estimates
+plot(eclosion_estimates) + xlab("Estimated Eclosion Rate")
+
+
+#Bar plot showing number of flies eclosed
+print(ggplot(etoa_data, aes(y=eclosed, x=treatment, fill = treatment)) 
+      + geom_col()
+      + theme_bw()
+      + ylab("Number of Flies Eclosed") 
+      + xlab("Lineage")
+)
+
+
+#Plot of mean and bootstrapped 95% confidence intervals
+print(ggplot(etoa_data, aes(x = treatment, y = eclosed)) 
+      + stat_summary(fun = mean, geom = "point", size = 3) 
+      + stat_summary(fun.data = mean_cl_boot, geom = "errorbar", width = 0.2) 
+      + theme_bw() 
+      + ylim(0,1)
+      + ylab("Proportion of Flies Eclosed") 
+      + xlab("Lineage")
+)
+
+
+Anova(etoa_eclosion)
+
+##############################################
+## Development Duration Analysis
 
 duration_data <- filter(etoa_data, eclosed == 1)
 head(duration_data)
@@ -43,4 +75,29 @@ check_model(etoa_duration)
 #Data points 284 and 283 are influential observations because the eclosed unusually late.
 #Should still be included in analysis in my opinion.
 
+#Inferential plots
+duration_estimates <- emmeans(etoa_duration, ~ treatment + sex, type = "response") 
+duration_estimates
+plot(duration_estimates) + xlab("Estimated Development Duration")
 
+emmip(duration_estimates, sex ~ treatment, CIs = TRUE) +
+  ylab("Estimated Development Duration") +
+  xlab("Lineage") +
+  theme_bw()
+
+
+print(ggplot(duration_data, aes(y=time, x=treatment, fill = treatment)) 
+      + geom_boxplot()
+      + theme_bw()
+      + ylab("Time of Eclosion") 
+      + xlab("Lineage")
+)
+
+print(ggplot(duration_data, aes(y=time, x=treatment:sex, fill = sex)) 
+      + geom_boxplot()
+      + theme_bw()
+      + ylab("Time of Eclosion") 
+      + xlab("Lineage")
+)
+
+Anova(etoa_duration)
--- scripts/Longevity_analysis.R
+++ scripts/Longevity_analysis.R
@@ -9,6 +9,7 @@ library(janitor)
 library(survminer)
 library(ggplot2)
 library(performance)
+library(emmeans)
 library(car)
 
 longev_data <- (read_excel("data/Longevity_Final.xlsx") 
@@ -37,17 +38,62 @@ check_model(m1) #does not work with cox mixed effects regression, it needs to be
 test_ph <- cox.zph(m1)
 ggcoxzph(test_ph)
 
-fit <- survfit(Surv(fly_lifespan, event=status) ~ treatment, data=longev_data) #fit for the purpose of making the figure
 
-ggplot1 <- ggsurvplot(fit, size = 2, censor = FALSE, legend = c(.25,.50),legend.title = "Treatment",
-                      legend.labs = c("Control","Selection"), ggtheme = theme(title = element_text(size=30), axis.title.x = element_text(size=30), axis.title.y = element_text(size=30),
-                                                                              legend.text = element_text(size=25), axis.text = element_text(size=18), 
-                                                                              legend.title = element_text(size=25), panel.background = element_rect(fill= FALSE),
-                                                                              axis.line = element_line(), plot.title = element_text(size=35, hjust = 0.5), 
-                                                                              legend.key.height = unit(1.3,'cm')
+#Inferential plots
+longev_estimates <- emmeans(m1, ~ sex + treatment, type = "response") 
+longev_estimates
+plot(longev_estimates) + xlab("Estimated Survival Response")
+
+emmip(longev_estimates, sex ~ treatment, CIs = TRUE) +
+  ylab("Estimated Survival Response") +
+  xlab("Lineage") +
+  theme_bw()
+
+
+#Plotting the effect of treatment without dividing up sexes
+treatment_fit <- survfit(Surv(fly_lifespan, event=status) ~ treatment, data=longev_data) #fit for the purpose of making the figure
+
+treatment_surv_curve <- ggsurvplot(treatment_fit, linewidth = 1, censor = FALSE, legend = c(.25,.50),
+                      legend.title = "Treatment",
+                      legend.labs = c("Control","Selection"), 
+                      ggtheme = theme(title = element_text(size=30), 
+                                      axis.title.x = element_text(size=30), 
+                                      axis.title.y = element_text(size=30),
+                                      legend.text = element_text(size=25), 
+                                      axis.text = element_text(size=18), 
+                                      legend.title = element_text(size=25), 
+                                      panel.background = element_rect(fill= FALSE),
+                                      axis.line = element_line(), 
+                                      plot.title = element_text(size=35, hjust = 0.5), 
+                                      legend.key.height = unit(1.3,'cm')
                       ))$plot
 
-ggplot1 + xlab("Age (Days)") + ylab("Proportion Alive")
+treatment_surv_curve + xlab("Age (Days)") + ylab("Proportion Alive")
+
+
+#Plotting the effect of treatment striated by sex
+sex_fit <- survfit(Surv(fly_lifespan, event=status) ~ treatment + sex, data=longev_data) #fit for the purpose of making the figure
+
+sex_surv_curve <- ggsurvplot(sex_fit, linewidth = 1, censor = FALSE, legend = c(.25,.50),
+                              legend.title = "Treatment",
+                              legend.labs = c("Control - F",'Control - M','Selection - F',"Selection - M"),
+                              ggtheme = theme(title = element_text(size=30), 
+                                  axis.title.x = element_text(size=30), 
+                                  axis.title.y = element_text(size=30),
+                                  legend.text = element_text(size=25), 
+                                  axis.text = element_text(size=18), 
+                                  legend.title = element_text(size=25), 
+                                  panel.background = element_rect(fill= FALSE),
+                                  axis.line = element_line(), 
+                                  plot.title = element_text(size=35, hjust = 0.5), 
+                                  legend.key.height = unit(1.3,'cm')
+                                   ))$plot
+
+sex_surv_curve + xlab("Age (Days)") + ylab("Proportion Alive") + ylim(0.9,1) 
+#Reduced the size of the y axis for better visualization.
+#However this figure may be misleading.
+
+Anova(m1)
 
 longev_data <- longev_data %>%
   dplyr::select(-specific)

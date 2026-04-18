a5d7d54080a897a50aa4ff2719cdcdb5192488da
jrosenbaum422 <jackrrosenbaum@gmail.com>
2026-04-18 17:04:19 -0400

    minor additions to mate choice analysis

--- scripts/MateChoice_analysis.R
+++ scripts/MateChoice_analysis.R
@@ -1,18 +1,21 @@
 library(DHARMa)
 library(performance)
-library(lme4)
+library(mclogit)
 
-mate_data <- readRDS('data/clean_matechoice.rds')
+
+raw_mate_data <- (read_excel("data/Mate choice FINAL.xlsx", sheet=2)
+                  |> clean_names()
+                  |> dplyr::select(day,arena,lineage_pair,s_male_mated)
+)
+
+#1 indicated the selection male mated, 0 indicates the control male mated
 
 summary(mate_data)
+
 
 mate <- glmer(data=mate_data, success ~ treatment + day + (1|lineage_pair), family = "binomial")
 
 check_model(mate)
 summary(mate)
 
-simulationOutput <- simulateResiduals(fittedModel = mate)
-testDispersion(simulationOutput)
-
-binned_residuals(mate)
 
